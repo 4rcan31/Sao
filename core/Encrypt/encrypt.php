c1393517e5a9c2f3727cc72eb34bf1038e6e3991
4rcan31 <rodrigo.francoescobar123@gmail.com>
2022-11-09 21:21:03 -0600

    Se implemento la app de encriptacion

--- core/Encrypt/encrypt.php
+++ core/Encrypt/encrypt.php
@@ -1,6 +1,62 @@
 <?php 
 
 
-class encrypt{
+class Encrypt extends Hasher{
+    protected $keyDefault = '123';
+
     
+    public function encrypt($string, $key){
+        $ivlen = openssl_cipher_iv_length("AES-128-CBC");
+        $iv = openssl_random_pseudo_bytes($ivlen);
+        $ciphertext_raw = openssl_encrypt(base64_encode($string), 'AES-128-CBC', $key, OPENSSL_RAW_DATA , $iv);
+        $hmac = hash_hmac('sha256', $ciphertext_raw, $key, true);
+        return $this->encode(base64_encode($iv.$hmac.$ciphertext_raw), $key.$this->keyDefault);
+    }
+
+    private function encode($string, $key){
+        $return = null;
+        for ($i = 0; $i < strlen($string); $i++) {
+            $char = substr($string, $i, 1);
+            $keychar = substr($key, ($i % strlen($key)) - 1, 1);
+            $char = chr(ord($char) + ord($keychar));
+            $return .= $char;
+        }
+        return base64_encode($return);
+    }
+    //FIN DE encrypt
+
+
+    //start decrypt
+    public function decrypt($string, $key){
+        $string = $this->decode($string, $key.$this->keyDefault);
+        $c = base64_decode($string);
+        $ivlen = openssl_cipher_iv_length($cipher="AES-128-CBC");
+        $iv = substr($c, 0, $ivlen);
+        $hmac = substr($c, $ivlen, $sha2len=32);
+        $ciphertext_raw = substr($c, $ivlen+$sha2len);
+        $original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+        $calcmac = hash_hmac('sha256', $ciphertext_raw, $key, true);
+        if (hash_equals($hmac, $calcmac)){
+            return base64_decode($original_plaintext);
+        }else{
+            return "¿Que intentas?";
+        }
+    }
+
+    private function decode($string, $key){
+        $return = null;
+        $string = base64_decode($string);
+        for ($i = 0; $i < strlen($string); $i++) {
+            $char = substr($string, $i, 1);
+            $keychar = substr($key, ($i % strlen($key)) - 1, 1);
+            $char = chr(ord($char) - ord($keychar));
+            $return .= $char;
+        }
+            if(is_numeric($return)) {
+                return intval($return);
+            } else {
+                return $return;
+            }
+    }
+    //end decrypt
 }
--- core/Encrypt/hasher.php
+++ core/Encrypt/hasher.php
@@ -2,6 +2,24 @@
 
 
 
-class hasher{
-    
+class Hasher{
+    public function hasher($PasswordInTextPlane){
+        $r = password_hash(
+            base64_encode(
+                hash('sha256', $PasswordInTextPlane, true)
+            ),
+            PASSWORD_DEFAULT
+        );
+        return $r;
+    }
+
+    public function verifyHash($PasswordInTextPlane, $hash){
+        $r = password_verify(
+            base64_encode(
+                hash('sha256', $PasswordInTextPlane, true)
+            ),
+            $hash
+        );
+        return $r;
+    }
 }
