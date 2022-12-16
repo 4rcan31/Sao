eb47ac957b1cc760c6a5a3b74c4ce2d4eab19ba6
4rcan31 <rodrigo.francoescobar123@gmail.com>
2022-12-15 21:51:08 -0600

    update layauts

--- core/Views/layouts.php
+++ core/Views/layouts.php
@@ -1,11 +1,11 @@
 <?php
-function importAllLayouts(){
-    import('layouts', false, 'app/Views/layouts');
+function layouts(){
+    import('Views/layouts', false);
 }
 
 
-function importLayout($file, $format = 'php'){
-    require_once dirname(__DIR__, 2)."/app/Views/layouts/".$file.".".$format;
+function layout($file, $format = 'php'){
+    import("/Views/layouts/$file.$format", false);
 }
 
 
