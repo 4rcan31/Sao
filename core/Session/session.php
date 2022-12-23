6d1e45af52b75ad35adc8abc47aeecc2a506a16a
4rcan31 <rodrigo.francoescobar123@gmail.com>
2022-12-22 19:11:46 -0600

    Delete some files

--- core/Session/session.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Session{  
-    public function cookie($name, $dato = '', $expirate = 0, $path = '', $domain = '', $secure = false, $httponly = false){
-        return setcookie($name, $dato, $expirate, $path, $domain, $secure, $httponly);
-    }
-}
