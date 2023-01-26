08d5f196442d8e61c67978a1e26988cf833a7309
4rcan31 <rodrigo.francoescobar123@gmail.com>
2023-01-26 01:10:59 -0600

    Update example routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
+//Si quieres ver la url visitada actualmente puedes ejecutar: Request::$uri;
 
-
-Router::get('/', function(){
+Route::get('/', function(){
     echo "This is the root";
 });
+
+
+
+Route::get('/home', function(){
+    controller('IndexController', 'home');
+});
+
+
+
+Route::group(function(){
+    Route::get('/', function(){
+        echo "This is the root from group with prefix /public";
+    });
+    
+    
+    
+    Route::get('home', function(){
+        echo "This is the /home from group with prefix /public";
+        controller('IndexController', 'home');
+    });
+    
+})->prefix('/public');
