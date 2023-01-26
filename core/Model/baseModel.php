67e5311e8e9462a21353c8635fe080eade25692f
4rcan31 <rodrigo.francoescobar123@gmail.com>
2023-01-26 01:02:00 -0600

    Update app baseModel, import orm here

--- core/Model/baseModel.php
+++ core/Model/baseModel.php
@@ -1,6 +1,7 @@
 <?php 
 
-import('/Database/ORM/orm.php', false, '/core');
+//import('/Database/ORM/orm.php', false, '/core');
+core('Database/ORM/orm.php', false);
 class BaseModel extends ORM{
     
 }
