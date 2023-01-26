40b456e8b376b962dd9224b78be99edc6c161d3e
4rcan31 <rodrigo.francoescobar123@gmail.com>
2023-01-26 01:07:54 -0600

    New rule contain, and new function err()

--- core/validate/validate.php
+++ core/validate/validate.php
@@ -4,14 +4,17 @@
 class Validate{
     public $datos;
     public $validates = [];
+    public $msg = [];
 
     function __construct($datos){
         $this->datos = $datos;    
     }
 
-    public function rule($rule, $campos){
+    public function rule($rule, $campos, $otros = []){
         if($rule === 'required'){
             array_push($this->validates, $this->required($campos));
+        }else if($rule === 'contain'){
+            array_push($this->validates, $this->contain($campos, $otros));
         }else{
             res('Not validate named: '.$rule);
         }
@@ -19,8 +22,26 @@ class Validate{
 
     public function required($campos){
         foreach($campos as $campo){
-            if(!isset($this->datos[$campo]) || empty($this->datos[$campo])){
+            if(!isset($this->datos[$campo])){
+                array_push($this->msg, "El campo '$campo' no existe.");
                 return false;
+            }
+            if((empty($this->datos[$campo]) && $this->datos[$campo] !== false) || $this->datos[$campo] === null){
+                array_push($this->msg, "El campo $campo esta vacio.");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public function contain($sentenses, $contains){
+        foreach($sentenses as $sentense){
+            $input = $this->input($sentense);
+            foreach($contains as $contain){
+                if(!str_contains($input, $contain)){
+                    array_push($this->msg, "$sentense no contiene $contain");
+                    return false;
+                }
             }
         }
         return true;
@@ -37,6 +58,13 @@ class Validate{
     }
 
     public function input($index){
-        return $this->datos[$index];
+        if(isset($this->datos[$index])){
+            return $this->datos[$index];
+        }
+        throw new Exception('El indice: "'.$index.'" no existe.'); 
+    }  
+
+    public function err(){
+        return $this->msg;
     }
 }
