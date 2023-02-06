5a0842d46999c847d9d4f93ddb20d3e8e813a56f
4rcan31 <rodrigo.francoescobar123@gmail.com>
2023-02-06 00:59:00 -0600

    Version 2 of the auth app - work it (in beta yet)

--- core/Auth/auth.php
+++ core/Auth/auth.php
@@ -1,19 +1,59 @@
 <?php
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+class Sauth {
+    private static $data;
+    public static $token = '';
 
-class Auth {
-    private static $data = [];
+    public static function token(Array $payload, string $key, $encrypter = true, $algori = 'HS256'){
+        $jwt = JWT::encode($payload, $key, $algori);
+        if($encrypter){
+            $jwt = core('Encrypt/encrypt.php')->encrypt($jwt, $key);
+        }
+        Sauth::$token = $jwt;
+        return $jwt;
+    }
 
-    public static function set(array $data){
-        Auth::$data = $data;
+    public static function decodeToken(string $token, string $key, $encrypter = true, $algori='HS256'){
+        if($encrypter){
+            $token = core('Encrypt/encrypt.php')->decrypt($token, $key);
+        }
+        $token  = JWT::decode($token, new Key($key, $algori));
+        /* Sauth::$data = $jwt; */
+        return $token;
     }
 
 
-    public static function data(){
-        return arrayToObject(Auth::$data);
+    public static function start(string $token, string $key){
+        try{
+            $token = Sauth::decodeToken($token, $key);
+        }catch(Exception $e){
+            $token = false;
+        }
+        if($token == false){
+            return $token;
+        }
+        Sauth::$data = $token;
+        return true;
+    }
+
+    public static function data(string $key = ''){
+        if(empty($key)){
+            return Sauth::$data;
+        }
+        if(isset(Sauth::$data->$key)){
+            return Sauth::$data->$key;
+        }else{
+            throw new Exception("El indice $key no existe");
+        }
     }
 
 
 }
+
+
+
+
 
 
 
--- core/Helpers/sao/auth.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-
-function jwt(){
-    return import('Auth/JwtSession.php', true, '/core');
-}
