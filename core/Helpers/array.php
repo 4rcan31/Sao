6ed225600900d2ac73306e1f7187504546685210
4rcan31 <rodrigo.francoescobar123@gmail.com>
2023-08-12 19:01:35 -0600

    Add new function called deleteElementArray, is similar to unset, but the funcion sort the indexes

--- core/Helpers/array.php
+++ core/Helpers/array.php
@@ -12,3 +12,16 @@ function sortIndex($array){ //Ordena los indices de un array
 function arrayToObject(Array $array){
     return json_decode(json_encode($array, JSON_FORCE_OBJECT));
 }
+
+function isAssoc( $array ) {
+	return array_keys( $array ) !== range( 0, count($array) - 1 );
+}
+
+function deleteElementArray(array $array, ...$indexs){
+    for($i = 0; count($indexs) > $i; $i++){
+        unset($array[$indexs[$i]]);
+    }
+    return sortIndex($array);
+}
+
+
