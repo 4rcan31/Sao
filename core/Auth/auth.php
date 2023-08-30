a0b0f4007d8f05b2963a85457c5e99d7f148bc9e
4rcan31 <rodrigo.francoescobar123@gmail.com>
2023-08-29 22:20:01 -0600

    Update module Auth (Sauth) in fase alfa

--- core/Auth/auth.php
+++ core/Auth/auth.php
@@ -1,24 +1,37 @@
 <?php
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
+
 class Sauth {
     private static $data;
     public static $token = '';
+    public static $time;
+    public static $nameSesion = 'session';
 
     public static function token(Array $payload, string $key, $encrypter = true, $algori = 'HS256'){
         $jwt = JWT::encode($payload, $key, $algori);
         if($encrypter){
             $jwt = core('Encrypt/encrypt.php')->encrypt($jwt, $key);
         }
-        Sauth::$token = $jwt;
         return $jwt;
     }
 
-    public static function decodeToken(string $token, string $key, $encrypter = true, $algori='HS256'){
+    public static function decode(string $token, string $key, $encrypter = true, $algori='HS256'){
         if($encrypter){
-            $token = core('Encrypt/encrypt.php')->decrypt($token, $key);
+           
+            $tokent = core('Encrypt/encrypt.php')->decrypt($token, $key);
+            echo "<br>================================================ <br>";
+            var_dump($token);
+            echo "<br>================================================ <br>";
         }
-        $token  = JWT::decode($token, new Key($key, $algori));
+        try {
+            $token  = JWT::decode($token, new Key($key, $algori));
+            echo "<br>================================================ <br>";
+            var_dump($token);
+            echo "<br>================================================ <br>";
+        } catch (\Throwable $th) {
+            $token = false;
+        }
         /* Sauth::$data = $jwt; */
         return $token;
     }
@@ -26,12 +39,10 @@ class Sauth {
 
     public static function start(string $token, string $key){
         try{
-            $token = Sauth::decodeToken($token, $key);
+            $token = Sauth::decode($token, $key);
+
         }catch(Exception $e){
-            $token = false;
-        }
-        if($token == false){
-            return $token;
+            return false;
         }
         Sauth::$data = $token;
         return true;
@@ -39,13 +50,60 @@ class Sauth {
 
     public static function data(string $key = ''){
         if(empty($key)){
-            return Sauth::$data;
+            return Sauth::$data->data;
         }
-        if(isset(Sauth::$data->$key)){
-            return Sauth::$data->$key;
+        if(isset(Sauth::$data->data->$key)){
+            return Sauth::$data->data->$key;
         }else{
             throw new Exception("El indice $key no existe");
         }
+    }
+
+    public static function set(Array $userData, string $key, int $timeInDays = 7, $encrypter = true,$algori = 'HS256'){
+        self::$time = time() + (86400 * $timeInDays);
+        self::$token = self::token([
+            'iat' => time(), // Tiempo que inició el token
+            'exp' => self::$time, // Tiempo que expirará el token 7 días
+            'data' => $userData
+        ], $key, $encrypter, $algori);
+        return self::$token;
+    }
+
+
+    public static function loginClient() {
+        try {
+            setcookie(self::$nameSesion, self::$token, self::$time, '/'); // La cookie expirará en 7 días
+        } catch (\Throwable $th) {
+            throw new Exception("Error interno", 1);
+        }
+
+    }
+
+    public static function loginServer(string $table, string $colum, int $id, $idColumName = 'id'){
+        import('DataBase/ORM/orm.php', false, '/core');
+        $db = new DataBase;
+        $db->prepare();
+        $db->select([$colum])->from($table)->where($idColumName, $id);
+        if(!$db->execute()->exist()){
+            throw new Exception("The user with id ".$id." dont exist", 1);
+        }
+        $db->prepare();
+        $db->update($table, [
+            $colum => self::$token
+        ])->where($idColumName, $id);
+        $db->execute();
+    }
+    
+
+    public static function middleware($key){
+        var_dump(Request::$cookies);
+        return isset(Request::$cookies[self::$nameSesion]) && self::start(Request::$cookies[self::$nameSesion], $key) ?
+                true : false;
+    }
+
+    public static function getToken(){
+        return isset(Request::$cookies[self::$nameSesion]) && self::start(Request::$cookies[self::$nameSesion], $_ENV['APP_KEY']) ?
+                Request::$cookies[self::$nameSesion] : null;
     }
 
 
