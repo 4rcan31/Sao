d7fc781e66300271629aa78bb2e5c024b71803cb
4rcan31 <rodrigo.francoescobar123@gmail.com>
2023-10-12 03:20:11 -0600

    Updated the Jenu library, added new methods such as a pretty console, and attempted to create a method for executing a Node.js process (still in development).

--- core/Console/console.php
+++ core/Console/console.php
@@ -4,13 +4,15 @@
 class Jenu{
 
     public static array $args = [];
+    public static string $command = '';
     public static array $commands = [];
     public static $date = "";
     public static array $commandsShouldBeExecuted = [];
 
 
     public static function set($args){
-        self::$args = $args;
+        self::$command = $args[1]; //El indice 1 es el comando
+        self::$args = deleteElementArray($args, 1, 0); //Aca se elimina el argumento "jenu" y el comando
         self::$date = date('D.d.M.Y_H.i.s');
     }
 
@@ -29,34 +31,56 @@ class Jenu{
     }
     
 
-    public static function print($text){
-        print($text."\n");
+    public static function print($text, $newLine = true){
+        $newLine ?  print($text."\n") :  print($text);
     }
 
-    public static function success(string $text){
-        consoleSuccess($text);
+    public static function printRow(int $len = 100){
+        $row = "";
+        for($i = 0; $i < $len; $i++){
+            $row = "=$row";
+        }
+        echo "\n$row\n";
+    }
+
+    public static function success(string $text, bool $start = true){
+        consoleSuccess($text, $start);
     }
 
     public static function error(string $text){
         consoleError($text);
+        exit(0);
     }
 
-    public static function warn(string $text){
-        consoleWarning($text);
+    public static function warn(string $text, bool $start = true){
+        consoleWarning($text, $start);
     }
 
-    public static function command(string $command, callable $execute){
+    public static function command(string $command, callable $execute, string $help = "undefained", $type = "undefained"){
         array_push(self::$commands, [
             'command' => $command,
-            'executed' => $execute
+            'executed' => $execute,
+            'help' => $help,
+            'type' => $type
         ]);
+    }
+
+    public static function get($needle, $error, $defaultValue = null){
+        if (isset(Jenu::$args[$needle])) {
+            return Jenu::$args[$needle];
+        }
+        if ($defaultValue !== null) {
+            return $defaultValue;
+        }
+        Jenu::error($error);
+        return 0;
     }
 
 
     public static function execute(string $command){
         for($i = 0; count(self::$commands) > $i; $i++){
             if(self::$commands[$i]['command'] == $command){
-                self::$commands[$i]['executed'](deleteElementArray(self::$args, 1, 0));
+                self::$commands[$i]['executed'](Jenu::$args);
                 return;
             }
         }
@@ -70,12 +94,113 @@ class Jenu{
             Verificar si solo el primer argumento es igual, 
             por que solo el primero es el comando, los demas solamente son argumentos del comando
             */
-            if(self::$commands[$i]['command'] == self::$args[1]){
-                self::$commands[$i]['executed'](deleteElementArray(self::$args, 1, 0));
+            if(self::$commands[$i]['command'] == Jenu::$command){
+                self::$commands[$i]['executed'](Jenu::$args);
                 return;
             }
         }
-        self::error("The command '".self::$args[1]."' not is definied");
+        Jenu::print("Help");
+        Jenu::execute('help');
+        Jenu::print("\n");
+        self::error("The command '".Jenu::$command."' not is definied");
+
+    }
+
+/*     public static function executeNodeProcess($nodeCommand, $processador = 'node') {
+        $nodeCommand = "$processador $nodeCommand";
+        // Configurar los descriptores de archivos
+        $descriptorspec = array(
+            0 => array("pipe", "r"), // stdin (lectura)
+            1 => array("pipe", "w"), // stdout (escritura)
+            2 => array("pipe", "w"), // stderr (escritura)
+        );
+    
+        // Iniciar el proceso Node.js en segundo plano
+        $process = proc_open($nodeCommand, $descriptorspec, $pipes);
+    
+        if (is_resource($process)) {
+            // Configurar los descriptores de archivo como no bloqueantes
+            stream_set_blocking($pipes[0], 0);
+            stream_set_blocking($pipes[1], 0);
+    
+            // Leer y mostrar la salida estándar de Node.js en tiempo real
+            while (true) {
+                // Verificar si hay datos disponibles desde Node.js
+                $output = fgets($pipes[1]);
+    
+                if ($output !== false) {
+                    Jenu::print($output, false);
+                    flush(); // Asegura que la salida se muestre en tiempo real
+                }
+    
+                // Verificar si hay datos disponibles desde la consola de PHP (stdin)
+                $input = fgets(STDIN);
+    
+                if ($input !== false) {
+                    // Enviar datos desde la consola de PHP al proceso Node.js
+                    fwrite($pipes[0], $input);
+                }
+    
+                // Obtener el estado del proceso Node.js
+                $status = proc_get_status($process);
+    
+                if (!$status['running']) {
+                    break;
+                }
+            }
+    
+            // Obtener la salida de error de Node.js
+            $outputError = stream_get_contents($pipes[2]);
+            fclose($pipes[0]);
+            fclose($pipes[1]);
+            fclose($pipes[2]);
+    
+            // Esperar a que el proceso Node.js termine
+            proc_close($process);
+    
+            if ($status['exitcode'] !== 0) {
+                Jenu::warn("Node say -> $outputError");
+            }
+        }
+    } */
+
+
+    //Esto no soporta entradas del cliente
+     public static function executeNodeProcess($nodeCommand, $processador = 'node') {
+        $nodeCommand = "$processador $nodeCommand";
+        // Configurar los descriptores de archivos
+        $descriptorspec = array(
+            0 => array("pipe", "r"), // stdin
+            1 => array("pipe", "w"), // stdout
+            2 => array("pipe", "w"), // stderr
+        );
+
+        // Iniciar el proceso Node.js en segundo plano
+        $process = proc_open($nodeCommand, $descriptorspec, $pipes);
+
+        if (is_resource($process)) {
+            // Cerrar la entrada estándar, ya que no la usamos
+            fclose($pipes[0]);
+
+            // Leer y mostrar la salida estándar de Node.js en tiempo real
+            while (!feof($pipes[1])) {
+                Jenu::print(fgets($pipes[1]), false);
+                flush(); // Asegura que la salida se muestre en tiempo real
+            }
+
+            fclose($pipes[1]);
+
+            // Obtener la salida de error de Node.js
+            $outputError = stream_get_contents($pipes[2]);
+            fclose($pipes[2]);
+
+            // Obtener el código de salida del proceso Node.js
+            $nodeExitCode = proc_close($process);
+
+            if ($nodeExitCode !== 0) {
+                Jenu::warn("Node.js say -> $outputError");
+            }
+        }
     }
 
 }
