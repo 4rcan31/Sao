771eb94a396093e9c90f8514be0fb6548102e81b
4rcan31 <rodrigo.francoescobar123@gmail.com>
2023-10-12 03:43:15 -0600

    Added new methods, such as 'input' and 'get input,' to set the hidden HTML input with a CSRF token, and updated the 'validateToken' method.

--- core/server/csrf.php
+++ core/server/csrf.php
@@ -8,7 +8,6 @@ class TokenCsrf{
     }
     
     public static function getTokenFromSession() {
-        session_start();
         if (!isset($_SESSION['csrf_token'])) {
             $_SESSION['csrf_token'] = self::generateToken();
         }
@@ -20,13 +19,19 @@ class TokenCsrf{
     }
 
     public static function input(){
-        echo '<input type="hidden" name="csrf_token" value="'.self::getTokenFromSession().'">';
+        echo self::getInput();
+    }
+
+    public static function getInput(){
+        return '<input type="hidden" name="csrf_token" value="'.self::getTokenFromSession().'">';
     }
     
     public static function validateToken($request) {
         if(isset($request['csrf_token'])){
-            session_start();
-            return isset($_SESSION['csrf_token']) && $_SESSION['csrf_token'] === $request['csrf_token'];
+            if(isset($_SESSION['csrf_token']) && $_SESSION['csrf_token'] === $request['csrf_token']){
+                unset($_SESSION['csrf_token']);
+                return true;
+            }
         }
         return false;
     }
