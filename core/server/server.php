b31ebd75d154d6d8c8880814f2a7ab1bed605767
4rcan31 <rodrigo.francoescobar123@gmail.com>
2023-10-12 03:44:13 -0600

    Update methods Serve

--- core/server/server.php
+++ core/server/server.php
@@ -6,30 +6,31 @@ class Server
 
 
 
-    public function RouteAbsolute2($route, $formato)
-    {
-        if ($formato == "php") {
-            if (empty($_SERVER['DOCUMENT_ROOT'])) {
-                $LINK = __DIR__ . "\\";
-            } else {
-                $LINK = $_SERVER['DOCUMENT_ROOT'] . "\\";
-            }
-        } else {
-            $LINK = (stripos($_SERVER['SERVER_PROTOCOL'], 'https') === true ? 'https://' : 'https://') . $_SERVER['HTTP_HOST'] . "/";
-        }
-
-        return $LINK . $route;
-    }
-
-    public function RouteAbsolute($route){
+    public static function RouteAbsoluteStatic($route = null, $host = null){
         $protocol = 'https';
-        if(!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on') {
-            $protocol = "http";
+        if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on') {
+            $protocol = 'http';
         }
-        $protocol = $protocol . "://";
-        $domain = $_SERVER['HTTP_HOST'];
-        return $protocol . $domain . "/" . $route;
-        // return $LINK.$route;
+        $protocol = $protocol . '://';
+        $host === null ?  $domain = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost' :  $domain = $host;
+        return $route == null ? $protocol . $domain."/" : $protocol . $domain . '/' . $route;
     }
+
+    public function RouteAbsolute($route = null, $host = null) {
+        $protocol = 'https';
+        if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on') {
+            $protocol = 'http';
+        }
+        $protocol = $protocol . '://';
+        $host === null ?  $domain = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : 'localhost' :  $domain = $host;
+        return $route == null ? $protocol . $domain."/" : $protocol . $domain . '/' . $route;
+    }
+
+    public static function redirect($route){
+        $route = trim($route, '/');
+        header("Location: ".self::RouteAbsoluteStatic($route, null));
+        exit;
+    }
+    
 
 }
