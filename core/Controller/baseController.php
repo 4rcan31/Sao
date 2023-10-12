5363e46a7fb3c0707909ad4214dfc35418911d63
4rcan31 <rodrigo.francoescobar123@gmail.com>
2023-10-12 03:23:36 -0600

    Added three new methods: 'validateCsrfTokenWithRedirection,' 'validateFieldsWithRedirection,' and 'host' to reduce code duplication in the controllers.

--- core/Controller/baseController.php
+++ core/Controller/baseController.php
@@ -2,5 +2,26 @@
 
 
 class BaseController{
+
+    public function validateCsrfTokenWithRedirection($request, $redirectTo){
+        csrf();
+        if (!TokenCsrf::validateToken($request)) {
+            Form::send($redirectTo, ['Su sesión ha expirado'], 'Error');
+        }
+        return;
+    }
+
+    public function validateFieldsWithRedirection(Array $errors, $redirectTo, $validator){
+        if(!$validator->validate()){
+            Form::send($redirectTo, $errors, 'Error');
+        }
+        return;
+    }
+
+    public function host(){
+        return $_ENV['APP_SERVER_CROQUETTE_HOST'].":".$_ENV['APP_SERVER_CROQUETTE_PORT'];
+    }
+    
+    
     
 }
