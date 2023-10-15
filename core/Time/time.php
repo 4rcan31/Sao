c67ed4023792a07b6c5a3ea1b58ec536a32ae00f
4rcan31 <rodrigo.francoescobar123@gmail.com>
2023-10-14 23:48:31 -0600

    Add new method called builtMessageAge and Optimize method calculateDaysDifference

--- core/Time/time.php
+++ core/Time/time.php
@@ -7,9 +7,9 @@ class Time{
      *
      * @param string $birthdate The birthdate in "YYYY-MM-DD" format.
      *
-     * @return int The calculated age in years.
+     * @return array The calculated age.
      */
-    public function calculateAgeFromBirthdate($birthdate) {
+    public function calculateAgeFromBirthdate(string $birthdate) {
         $birthdate = new DateTime($birthdate);
         $now = new DateTime(date("Y-m-d"));
         $diff = $now->diff($birthdate);
@@ -19,40 +19,36 @@ class Time{
             'days' => $diff->d,
         ];
     }
+
+    public function builtMessageAge(array $date){
+        $messageAge = "";
+        if ($date['years'] > 0) {
+            $messageAge .= $date['years'] . ' año' . ($date['years'] > 1 ? 's' : '') . ', ';
+        }
+        if ($date['months'] > 0) {
+            $messageAge .= $date['months'] . ' mes' . ($date['months'] > 1 ? 'es' : '') . ', ';
+        }
+        if ($date['days'] > 0) {
+            $messageAge .= " y ".$date['days'] . ' día' . ($date['days'] > 1 ? 's' : '') . ' ';
+        }
+        return $messageAge;
+    }
     
 
 
 
     //formato ==> (fecha ahora, fecha a calcular) Y-d-m 
-    function CalcDays($Fecha, $Today){
- 
-        //defino fecha 1
-        $ano1 = explode("-", $Fecha)[0];
-        $mes1 = explode("-", $Fecha)[2];
-        $dia1 = explode("-", $Fecha)[1];
-
-        //defino fecha 2
-        $ano2 = explode("-", $Today)[0];
-        $mes2 =explode("-", $Today)[2];
-        $dia2 = explode("-", $Today)[1];
-
-        //calculo timestam de las dos fechas
-        $timestamp1 = mktime(0, 0, 0, $mes1, $dia1, $ano1);
-        $timestamp2 = mktime(4, 12, 0, $mes2, $dia2, $ano2);
-
-        //resto a una fecha la otra
-        $segundos_diferencia = $timestamp1 - $timestamp2;
-        //echo $segundos_diferencia;
-
-        //convierto segundos en días
-        $dias_diferencia = $segundos_diferencia / (60 * 60 * 24);
-
-        //obtengo el valor absoulto de los días (quito el posible signo negativo)
-        $dias_diferencia = abs($dias_diferencia);
-
-        //quito los decimales a los días de diferencia
-        $dias_diferencia = floor($dias_diferencia);
-
-        echo $dias_diferencia;
+    public function calculateDaysDifference(string $startDate, string $endDate, string $dateFormat = 'Y-m-d') {
+        $start = DateTime::createFromFormat($dateFormat, $startDate);
+        $end = DateTime::createFromFormat($dateFormat, $endDate);
+    
+        if ($start && $end) {
+            $interval = $start->diff($end);
+            return $interval->days;
+        }
+        return 0; // En caso de error, por ejemplo, si las fechas son inválidas
     }
+    
+
+    
 }
