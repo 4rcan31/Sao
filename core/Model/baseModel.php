92c327d4f31d4899ffc080d4dd98d8394ce6c453
4rcan31 <rodrigo.francoescobar123@gmail.com>
2023-11-18 03:05:52 -0600

    Add new method for join two tables, the method call getManyToManyRelationship

--- core/Model/baseModel.php
+++ core/Model/baseModel.php
@@ -3,5 +3,24 @@
 //import('/Database/ORM/orm.php', false, '/core');
 core('Database/ORM/orm.php', false);
 class BaseModel extends DataBase{
+
+
+    public function getManyToManyRelationship(string $relatedTable, string $pivotTable,string $primaryKeyColumnPivot = null, string $primaryKeyColumnRelated = 'id') {
+        if ($primaryKeyColumnPivot === null) {
+            // Si no se proporciona un nombre de columna de pivote, asumimos una convención específica
+            $pivotTableParts = explode('_', $pivotTable);
+            $primaryKeyColumnPivot = $pivotTableParts[0] . '_id'; // Por ejemplo, user_id si el nombre de la tabla es users_has_roles
+        }
+    
+        // Construir la consulta para recuperar los registros relacionados
+        $this->prepare();
+        return $this->select([$relatedTable.'.*'])
+            ->from($relatedTable)
+            ->join($pivotTable)
+            ->on($relatedTable . '.' . $primaryKeyColumnRelated, $pivotTable . '.' . $primaryKeyColumnPivot);
+    
+        
+    }
+    
     
 }
